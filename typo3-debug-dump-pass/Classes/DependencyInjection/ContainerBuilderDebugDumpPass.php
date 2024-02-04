2886fbcd1e3de49333a7c1041063b77cf19a37dd
Sebastian Schreiber <me@schreibersebastian.de>
2024-02-05 00:42:49 +0100

    [TASK] Add compiler pass

---
+++ typo3-debug-dump-pass/Classes/DependencyInjection/ContainerBuilderDebugDumpPass.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Ssch\Typo3DebugDumpPass\DependencyInjection;
+
+use Symfony\Component\Config\ConfigCache;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\XmlDumper;
+use TYPO3\CMS\Core\Core\Environment;
+
+final class ContainerBuilderDebugDumpPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $environment = $this->resolveEnvironmentName();
+        $cache = new ConfigCache(Environment::getVarPath() . '/cache/' . strtolower($environment) . '/App_Kernel'.$environment.'DebugContainer.xml', true);
+        if (!$cache->isFresh()) {
+            $cache->write((new XmlDumper($container))->dump(), $container->getResources());
+        }
+    }
+
+    private function resolveEnvironmentName()
+    {
+        if(Environment::getContext()->isProduction()) {
+            return 'Production';
+        }
+
+        if(Environment::getContext()->isDevelopment()) {
+            return 'Development';
+        }
+
+        if(Environment::getContext()->isTesting()) {
+            return 'Testing';
+        }
+
+        throw new \UnexpectedValueException('No context given');
+    }
+}
---
+++ typo3-debug-dump-pass/Configuration/Services.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+use Ssch\Typo3DebugDumpPass\DependencyInjection\ContainerBuilderDebugDumpPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use TYPO3\CMS\Core\Core\Environment;
+
+return static function (ContainerConfigurator $container, ContainerBuilder $containerBuilder) {
+
+    if(!Environment::getContext()->isProduction()) {
+        $containerBuilder->addCompilerPass(new ContainerBuilderDebugDumpPass(), PassConfig::TYPE_BEFORE_REMOVING, -255);
+    }
+};
