a7dc05450e234b3da42edcbcaf5022e1546dcf0e
Sebastian Schreiber <me@schreibersebastian.de>
2024-02-05 20:40:28 +0100

    [TASK] Remove guard clause in Services.php

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -6,10 +6,7 @@ use Ssch\Typo3DebugDumpPass\DependencyInjection\ContainerBuilderDebugDumpPass;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use TYPO3\CMS\Core\Core\Environment;
 
 return static function (ContainerConfigurator $container, ContainerBuilder $containerBuilder) {
-    if (!Environment::getContext()->isProduction()) {
-        $containerBuilder->addCompilerPass(new ContainerBuilderDebugDumpPass(), PassConfig::TYPE_BEFORE_REMOVING, -255);
-    }
+    $containerBuilder->addCompilerPass(new ContainerBuilderDebugDumpPass(), PassConfig::TYPE_BEFORE_REMOVING, -255);
 };
